4316d54602c6d456810ff2f14fc457fcd55dce39
kpaje <kelvinmembership@gmail.com>
2019-11-12 18:19:58 -0800

    Create a stack data structure.  The stack should be a class with methods push, pop, and peek.  Adding an element to the stack should store it until it is removed.

--- _notes/dataStructures.js
+++ _notes/dataStructures.js
@@ -4,7 +4,12 @@
 //QUEUE
 // Linear processing like standing in line for a ticket
 // No skipping or cutting in line
-
 // Enqueuing = adding
 // Dequeuing = removing
 // FIFO = First In First Out
+
+//STACKS
+// Ordered list of records like a queue
+// Push = adding
+// Pop = removing
+// FILO = First In First Out; last record in is last record out
