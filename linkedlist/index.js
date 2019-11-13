476d0dd10d64f9664740d9bab2bac5d8c82c08d0
kpaje <kelvinmembership@gmail.com>
2019-11-12 23:12:29 -0800

    created size() method to return the number of nodes in a linked list

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -19,6 +19,18 @@ class LinkedList {
     this.head = node;
     // this.head = new Node(data, this.head); //refactored
   }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
 }
 
 module.exports = { Node, LinkedList };
