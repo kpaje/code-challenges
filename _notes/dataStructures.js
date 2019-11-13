990cacc30be13e3f1274320d1fe3895423cf95f6
kpaje <kelvinmembership@gmail.com>
2019-11-12 17:57:37 -0800

    Create a queue data structure.  The queue should be a class with methods add and remove. Adding to the queue should store an element until

---
+++ _notes/dataStructures.js
@@ -0,0 +1,10 @@
+//DATA STRUCTURES
+// Ways of organinzing information with optimal 'runtime complexity' for adding or removing records
+
+//QUEUE
+// Linear processing like standing in line for a ticket
+// No skipping or cutting in line
+
+// Enqueuing = adding
+// Dequeuing = removing
+// FIFO = First In First Out
