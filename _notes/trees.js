feb3b33988cc83f001fbc5302668e63a21bec33b
kpaje <kelvinmembership@gmail.com>
2019-11-20 11:40:17 -0800

    tree notes overview

---
+++ _notes/trees.js
@@ -0,0 +1,12 @@
+//TREES!
+// A node holds data and a reference to any of it's children
+// children = sub nodes
+// siblings = same level but doesn't necessarily have the same parent
+// traversal = iterating through a tree
+
+//BREADTH-FIRST TRAVERSAL
+// Starting from the root node, iterate through each level of a tree from left to right
+
+//DEPTH-FIRST TRAVERSAL
+// starting from the root node, iterate through  the very left side down to the bottom of a parent-child relationship
+// trying to reach the end of a tree as fast as possible
